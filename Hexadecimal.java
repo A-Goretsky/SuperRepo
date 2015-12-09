01c5b4f57896df32ef6b0723efb90183258e3154
Anton Goretsky <agoretsky@stuy.edu>
2015-12-09 11:46:13 -0500

    Commit of original files.

---
+++ Hexadecimal.java
@@ -0,0 +1,126 @@
+//Anton Goretsky
+//APCS1 pd5
+//HW44 - This or That or Fourteen Other Things
+//2015-12-8
+
+public class Hexadecimal {
+    
+//instance vars
+    private final static String HEXDIGITS = "0123456789ABCDEF";
+    public int _decNum;
+    public String _hexNum;
+
+//default constructor
+    public Hexadecimal() {
+        _hexNum = "";
+        _decNum = 0;
+    }
+
+//overloaded constructor for dec input
+    public Hexadecimal(int n) {
+        _hexNum = decToHex(n);
+        _decNum = n;
+    }
+    
+//overloaded constructor for hex input
+    public Hexadecimal(String s) {
+        _hexNum = s;
+        _decNum = hexToDec(s);
+    }
+    
+//toString method
+    public String toString() {
+        return _hexNum;
+    }
+
+//decimal to hex, iteratively
+    public static String decToHex(int n) {
+        String ans ="";
+        while (n != 0) {
+            ans = HEXDIGITS.charAt(n % 16) + ans;
+			n /= 16;
+		}
+		return ans;    
+    }
+    
+//decimal to hex, recursively
+    public static String decToHexR(int n) {
+		String ans = "";
+		if (n != 0) {
+			ans += decToHexR(n / 16) + HEXDIGITS.charAt(n % 16);
+		}
+		return ans;
+    }
+
+//hex to decimal, iteratively
+    public static int hexToDec(String s) {
+        int ans = 0;
+		for (int x = s.length() - 1; x >= 0; x--) {
+			ans += (int) (HEXDIGITS.indexOf(s.charAt(0)) * Math.pow(16, x));
+			s = s.substring(1);
+		}
+		return ans;
+    }
+  
+//hex to decimal, recursively
+    public static int hexToDecR(String s) {
+		int ans = 0;
+		if (s.length() != 0) {
+			ans += ((int) (HEXDIGITS.indexOf(s.charAt(0)) * Math.pow(16, s.length() - 1))) + hexToDecR(s.substring(1));
+		}
+		return ans;
+    }
+	
+//equals override
+	public boolean equals(Object other) {
+		return (this == other || this.compareTo(other) == 0);
+	}
+	
+//compareTo method
+	public int compareTo(Object other) {
+		if (!(other instanceof Hexadecimal))
+			throw new ClassCastException("\n Error: compareTo() input was not of class Hexadecimal!");
+        return _decNum - ((Hexadecimal) other)._decNum;
+	}
+    
+	
+	//main method for testing
+    public static void main( String[] args ) {
+	//Hexadecimal h1 = new Hexadecimal("10FFD"); //69629
+	System.out.println(hexToDec("10FD")); //4349
+
+	System.out.println();
+	System.out.println( "Testing ..." );
+
+	Hexadecimal h1 = new Hexadecimal(26);
+	Hexadecimal h2 = new Hexadecimal(26);
+	Hexadecimal h3 = h1; // new Hexdecimal(5)
+	Hexadecimal h4 = new Hexadecimal("10FFD"); //69629
+	//System.out.println(hexToDec("10FD")); //4349
+
+	System.out.println( h1 );//1A
+	System.out.println( h2 );//1A
+	System.out.println( h3 );//1A
+	System.out.println( h4 );//10FFD
+	System.out.println( "\n==..." );
+	System.out.println( h1 == h2 ); //should be false
+	System.out.println( h1 == h3 ); //should be true
+
+	System.out.println( "\n.equals()..." );
+	System.out.println( h1.equals(h2) ); //should be true
+	System.out.println( h1.equals(h3) ); //should be true
+	System.out.println( h3.equals(h1) ); //should be true
+	System.out.println( h4.equals(h2) ); //should be false
+	System.out.println( h1.equals(h4) ); //should be false
+
+	System.out.println( "\n.compareTo..." );
+	System.out.println( h1.compareTo(h2) ); //should be 0
+	System.out.println( h1.compareTo(h3) ); //should be 0
+	System.out.println( h1.compareTo(h4) ); //should be neg
+	System.out.println( h4.compareTo(h1) ); //should be pos*/
+
+	//invalid object test
+	Object stuff = new Object();
+	System.out.println(h1.equals(stuff)); //should return new error.
+	}
+}
---
+++ Rational.java
@@ -0,0 +1,158 @@
+//Team Pip-Boys: Shariar Kabir, Anton Goretsky
+//APCS1 pd5
+//HW #41: In America, the Driver Sits on the Left
+//2015-11-17
+
+
+public class Rational implements Comparable
+ {
+  //instance variables for class Rational
+  private int numerator;
+  private int denominator;
+  //=======================================================================================
+  //constructors
+  public Rational(){ //default constuctor sets value of instance to 0/1
+    numerator=0;
+    denominator=1;
+  }
+  public Rational(int num, int den){
+    if (den==0){
+      numerator=0; //resets value of instance to 0/1 if denominator is 0
+      denominator=1;
+      System.out.println("Please enter non-zero denominator!");
+    }
+    else{ //redefine values of instance
+      numerator=num;
+      denominator=den;
+    }
+  }
+  //=======================================================================================
+  //methods
+  public String toString(){ //returns a string representation of the rational number
+    String rationalNum = "" ;
+    
+    rationalNum += numerator;
+    rationalNum += "/" ;
+    rationalNum += denominator;
+    return rationalNum; //format: "numerator/denominator"
+  }    
+  public double floatValue(){ //returns a floating point value of nuumber
+    double returnValue; //using the most precise floating point primitive
+    
+    returnValue = ((double)numerator)/denominator;
+    //calculates the rational number with precision by converting the first int
+    //to a double, thus making the end value have double precision
+    return returnValue;
+  }  
+  public void multiply(Rational frac){ //Rational object * this Rational object
+    numerator *= frac.numerator; //multiplies numerators
+    denominator *= frac.denominator; //multiplies denominators
+  }     
+  public void divide(Rational frac){ // Rational object / this Rational object
+    numerator *= frac.denominator; //multiplies numerator by divisor's denominator
+    denominator *= frac.numerator; //multiplies denominator by divisor's numerator 
+  }
+  public void add(Rational frac){ //Rational object + this Rational object
+    if (denominator == frac.denominator){ 
+      numerator += frac.numerator; //add numerators if denominators are equal
+    }
+    else{ //cross multiply and simplify
+      int objectNumerator = numerator*frac.denominator;
+      int thisNumerator = frac.numerator*denominator;
+      
+      numerator = objectNumerator + thisNumerator;
+      denominator *= frac.denominator;
+    }
+  }
+  public void subtract(Rational frac){ //Rational object - this Rational object
+    if (denominator == frac.denominator){
+      numerator -= frac.numerator; //subtract numerators if denominators are equal
+    }
+    else{ //cross multiply and simplify
+      int objectNumerator = numerator*frac.denominator;
+      int thisNumerator = frac.numerator*denominator;
+      
+      numerator = objectNumerator - thisNumerator;
+      denominator *= frac.denominator;
+    }
+  }
+  public int gcd(int a, int b){ //finds greatest common divisor between a and b
+    int largerNum;
+    int currentGCD;
+    int remainder;
+    
+    if (a > b){ //determines larger of the two numbers
+      largerNum = a;
+      currentGCD = b;
+      remainder = a % b;
+    }
+    else{
+      largerNum = b;
+      currentGCD = a;
+      remainder = b % a;
+    }
+    
+    while (remainder > 0){ //applies Euclid's algorithm
+      largerNum = currentGCD;
+      currentGCD = remainder;
+      remainder = largerNum % currentGCD;
+    }
+    
+    return currentGCD;
+  }
+  public void reduce(){ //reduces this Rational to lowest terms
+    int gcd = gcd(this.numerator, this.denominator);
+    
+    numerator /= gcd;
+    denominator /= gcd;
+  }
+  public int compareTo(Object frac){
+    Rational temp = (Rational)frac;
+    if (((this.numerator *((Rational)frac).denominator) >(((Rational)frac).numerator* this.denominator)))
+      return 1;
+    if (((this.numerator *((Rational)frac).denominator) <(((Rational)frac).numerator* this.denominator)))
+      return -1;
+    return 0;
+  }
+  public boolean equals(Object frac){
+    boolean retVal;
+    //check for aliasing
+    retVal = this == (Rational)frac;
+    if (!retVal){ //if this and frac are not the same object
+      int x = compareTo(frac);
+      if (x == 0){retVal=true;}
+      else{retVal = false;}
+    }
+    return retVal;}
+  
+  //=======================================================================================
+  //main method
+  public static void main (String[] args) { 
+    Rational r = new Rational(2,4); 
+    Rational s = new Rational(1,2);
+    System.out.println(r.equals(s)); //true
+    Rational x = new Rational(1,3);
+    System.out.println(r.equals(x)); //false
+     System.out.println(r); //(2/3)
+     System.out.println(s); //(1/2)
+     r.add(s); //(2/3)+(1/2)
+     System.out.println(r); //(7/6)
+     System.out.println(s); //(1/2)
+     r.subtract(s); //(7/6)-(1/2)
+     System.out.println(r); //(8/12)
+     System.out.println(s); //(1/2)
+     r.reduce();//(8/12) -> (2/3)
+     System.out.println(r);//(2/3)
+     System.out.println(s.floatValue()); //should output 0.5
+     System.out.println(r); //should output 2/3
+     System.out.println(s); //should output 1/2 
+     r.multiply(s); //(2/3)*(1/2) 
+     System.out.println(r); //should output 2/6 
+     System.out.println(s); //should output 1/2 
+     r.divide(s); //(2/6)/(1/2) 
+     System.out.println(r); //should output 4/6 
+     System.out.println(s); //should output 1/2 
+
+  } //end main
+  
+} //end Rational
